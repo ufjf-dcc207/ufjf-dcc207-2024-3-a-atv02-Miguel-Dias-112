3751312e39d94b8235d14962e78607a378606637
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-11-19 15:18:43 -0300

    adiciona estilo para a view de dados

--- src/Components/DadosAlunos/DadosAlunos.tsx
+++ src/Components/DadosAlunos/DadosAlunos.tsx
@@ -1,34 +1,18 @@
 import { Aluno } from '../../App'
-
-function Dado({nome, valor}: {nome: string, valor: string | number}) {
-    return (
-     <div>
-       <h1>{nome}</h1>
-       <p>{valor}</p>
-     </div>
-    )
- }
- 
+import './DadosAluno.css'
  function DadosAluno(aluno: Aluno) {
     return (
-      <div>
+      <div className='dadosContainer'>
         {Object.entries(aluno).map(([nome, valor]) => {
-          return <Dado nome={nome} valor={valor} />
+          return (
+            <div className='dadosData'>
+              <h1>{nome}</h1>
+              <p>{valor}</p>
+            </div>
+          )
+          
         })}
-        <div>
-          <h1>Matricula</h1>
-          <h1>Situação</h1>
-          <h1>Nome</h1>
-          <h1>Curso</h1>
-          <h1>Periodo</h1>
-        </div>
-        <div>
-          <p>{aluno.matricula}</p>
-          <p>{aluno.idade}</p>
-          <p>{aluno.nome}</p>
-          <p>{aluno.curso}</p>
-          <p>{aluno.periodoLetivo}</p>
-        </div>
+       
   
       </div>
     )
